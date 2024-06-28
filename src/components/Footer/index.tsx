897706d29f7cb6e54c7a9691d9fa16147240386f
shoonia <alexanderz@wix.com>
2024-06-28 18:30:52 +0300

    Style footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,22 @@
-import { _footer } from './styles.css';
+import { _box, _auth } from './styles.css';
 import cba from './images/cba.svg';
 import fp from './images/fp.svg';
 import u24 from './images/u24.svg';
 
 export const Footer: JSX.FC = () =>
-  <footer class={_footer}>
-    <a href="https://savelife.in.ua/donate/">
-      <img src={cba} width="200" alt="Повернись Живим — фонд компетентної допомоги армії" />
-    </a>
-    <a href="https://prytulafoundation.org/donation">
-      <img src={fp} height="100" alt="благодійний фонд Сергія Притули" />
-    </a>
-    <a href="https://u24.gov.ua/uk/donate">
-      <img src={u24} width="200" alt="United24 - офіційна фандрейзингова платформа України" />
-    </a>
+  <footer>
+    <div class={_box}>
+      <a href="https://savelife.in.ua/donate/">
+        <img src={cba} width="100" alt="Повернись Живим — фонд компетентної допомоги армії" />
+      </a>
+      <a href="https://prytulafoundation.org/donation">
+        <img src={fp} height="50" alt="благодійний фонд Сергія Притули" />
+      </a>
+      <a href="https://u24.gov.ua/uk/donate">
+        <img src={u24} width="100" alt="United24 - офіційна фандрейзингова платформа України" />
+      </a>
+    </div>
+    <div class={_auth}>
+      <time dateTime="2024">2024</time>
+    </div>
   </footer>;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,5 +4,7 @@ import logo from './logo.svg';
 
 export const Header: JSX.FC = () =>
   <header class={_header}>
-    <img src={logo} width="100" alt="Шо не Yasno?" />
+    <a href="./" aria-current="page">
+      <img src={logo} width="100" alt="Шо не Yasno?" />
+    </a>
   </header>;
