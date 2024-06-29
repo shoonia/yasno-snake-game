0e4d33c2ce37dc572dc6023c80cbf850436b9315
shoonia <alexanderz@wix.com>
2024-06-29 15:35:03 +0300

    Update styles

--- src/components/Game/Time.tsx
+++ src/components/Game/Time.tsx
@@ -6,8 +6,10 @@ import { X } from './consts';
 export const Time: JSX.FC = () =>
   <div class={s.row}>
     {from(X, (_, i) =>
-      <div class={s.time}>
-        {i > 9 ? i : '0' + i}:00
+      <div class={s.cellt}>
+        <span class={s.label}>
+          {i > 9 ? i : '0' + i}:00
+        </span>
       </div>,
     )}
   </div>;
